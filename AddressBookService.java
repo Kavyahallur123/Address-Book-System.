e458daa2e83bb8a03832e99cc2f44458faffeaf6
kavyahallur123 <kavyahallur199@gmail.com>
2022-11-06 13:39:14 +0530

    Added: No duplicate entry of the same person

--- AddressBookService.java
+++ AddressBookService.java
@@ -1,2 +1,49 @@
-package com.bridgelabz.AddressBook;public class AddressBookService {
+package com.bridgelabz.AddressBook;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AddressBookService {
+    private ArrayList<Contact> contactList = new ArrayList<>();
+    public boolean addContact(Contact contact) {
+        List<Contact> filteredByFName = searchByName(contact.getfName());
+        for (Contact sameName : filteredByFName)
+            if (sameName.equals(contact))
+                return false;
+        contactList.add(contact);
+        return true;
+    }
+
+    public List<Contact> searchByName(String name){
+        return contactList.stream().
+                filter(person -> person.getfName().equalsIgnoreCase(name)).
+                collect(Collectors.toList());
+    }
+
+    public boolean editContact(Contact current, Contact modified) {
+        if(!contactList.contains(current))
+            return false;
+        contactList.remove(current);
+        contactList.add(modified);
+        return true;
+    }
+
+    public boolean deleteContact(Contact contact) {
+        contactList.remove(contact);
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        if (contactList.isEmpty())
+            return "No contacts found!";
+
+        StringBuilder builder = new StringBuilder();
+        for (Contact contacts : contactList)
+            builder.append(contacts.toString()).append("\n");
+
+        return builder.toString();
+    }
+
 }
